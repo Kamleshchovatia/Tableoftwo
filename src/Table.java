73fa7e90063d76b4a94d03a3737a1a25ecfe0a62
Kamlesh Chovatia <kamlesh.chovatia19@gmail.com>
2022-05-02 20:12:29 +0100

    first commit

---
+++ src/Table.java
@@ -0,0 +1,16 @@
+public class Table {
+
+    public static void main(String[] args) {
+
+        for (int a = 2; a <=2 ; a++) {
+
+            for (int b = 1; b <=10 ; b++) {
+
+                System.out.println(a+ " X " +b+ " = " + (a*b));
+
+            }
+
+        }
+
+    }
+}
